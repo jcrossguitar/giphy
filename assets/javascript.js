3e56447920c4640745cda894d945a13baefc8f55
jon <jcrossguitar@gmail.com>
2019-08-16 03:14:08 -0700

    I was having trouble with submitting the text for a response. I resolved that and it is working now, but it keeps redirecting me to a new tab upon each click. Also i want to display the object on the page, but have not been able to get that yet.

--- assets/javascript.js
+++ assets/javascript.js
@@ -19,25 +19,34 @@ for (var i = 0; i < topics.length; i++) {
 
                 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=10");
                     xhr.done(function(data) { console.log("success got data", data); });
-                // if the city value is not blank, run ajax call to request information from the server
                  
-                // ajax call
-                                // $.ajax({
-                                //     url: "https://api.giphy.com/v1/gifs/" + topics + "&?api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&tag",
-                                //     type: 'GET',
-                                //     dataType: "jsonp",
-                                //     success: function(data){
-                                //         console.log(data)
-                                //     }
-                                // })
-                // display an error if form submitted blank
-                            
                             // this area is my function for creating buttons, but is not working at the moment
                             function myFunction() {
                                 var btn = document.createElement("#topics");
                                 btn.innerHTML = "";
                                 document.body.appendChild(buttons);
                               }
+                
+                        });
+                    });
+// this area uses the api to search based on the term input into the search. an object has been captured by clicking the search button
+
+        $(document).ready(function() {
+            $("#inputBtn").click(function(){
+                            console.log('search...')
+                            // creating a variable for search term
+                var search = $("#topic").val();
+        
+
+                var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=10");
+                    xhr.done(function(data) { console.log("success got data", data); });
+                 
+                            // this area is my function for creating buttons, but is not working at the moment
+                            $("#inputBtn").click(function myFunction() {
+                                var btn = document.createElement("#topic");
+                                btn.innerHTML = "";
+                                document.body.appendChild(buttons);
+                              });
                 
                         });
                     });
