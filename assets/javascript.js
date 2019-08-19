b8475342326f45919ce88673bbc4b2cb06d0aebd
jon <jcrossguitar@gmail.com>
2019-08-19 12:58:06 -0700

    I was able to create a row to display the gif object, but have been unable to grab the giphs themselves

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,14 @@
+
+
+var topics = ["dog", "cat", "zebra", "horse", "monkey", "gorilla", "fish", "seal", "sea lion",
+"snake", "rabbit", "coyote", "duck", "bird", "shark", "frog", "marsupial", "thylacine", "wolf", "bear",
+"elephant"]
+// this area creates a for loop that adds buttons for each "topics" value
+for (var i = 0; i < topics.length; i++) { 
+    var buttons = $('<button>'+ topics[i] + '</button>') 
+    buttons.appendTo('#topics'); 
+} 
+
 var createRow = function(data) {
     // Create a new table row element
     var tRow = $("<tr>");
@@ -13,15 +24,6 @@ var createRow = function(data) {
     $("tbody").append(tRow);
   };
 
-var topics = ["dog", "cat", "zebra", "horse", "monkey", "gorilla", "fish", "seal", "sea lion",
-"snake", "rabbit", "coyote", "duck", "bird", "shark", "frog", "marsupial", "thylacine", "wolf", "bear",
-"elephant"]
-// this area creates a for loop that adds buttons for each "topics" value
-for (var i = 0; i < topics.length; i++) { 
-    var buttons = $('<button>'+ topics[i] + '</button>') 
-    buttons.appendTo('#topics'); 
-}         
-
 
 
 // creates a click function for clicking the buttons
@@ -32,7 +34,7 @@ for (var i = 0; i < topics.length; i++) {
                 var topic = $("#topic").val();
         
 
-                var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=5");
+                var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=10");
                  
                     xhr.done(function(data) { console.log("success got data", data); })
                     .then(function(response) {
@@ -40,11 +42,11 @@ for (var i = 0; i < topics.length; i++) {
                     });
                  
                             // this area is my function for creating buttons, but is not working at the moment
-                            function myFunction() {
-                                var btn = document.createElement("#topics");
-                                btn.innerHTML = "";
-                                document.body.appendChild(buttons);
-                              }
+                            // function myFunction() {
+                            //     var btn = document.createElement("#topics");
+                            //     btn.innerHTML = "";
+                            //     document.body.appendChild(buttons);
+                            //   }
                 
                         });
                     });
@@ -57,17 +59,22 @@ for (var i = 0; i < topics.length; i++) {
                             console.log('search...')
                             // creating a variable for search term
                 var search = $("#topic").val();
+             // this area is my function for creating buttons, but is not working at the moment
+                    $("#inputBtn").click(function myFunction() {
+                        var btn = document.createElement("#topic");
+                        btn.innerHTML = "";
+                        document.body.appendChild(buttons);
+                    });
         
 
                 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=10");
-                    xhr.done(function(data) { console.log("success got data", data); });
+                    xhr.done(function(data) { console.log("success got data", data); })
+                    .then(function(response) {
+                        createRow(response);
+                    });
                  
                             // this area is my function for creating buttons, but is not working at the moment
-                            $("#inputBtn").click(function myFunction() {
-                                var btn = document.createElement("#topic");
-                                btn.innerHTML = "";
-                                document.body.appendChild(buttons);
-                              });
+
                 
                         });
                     });
