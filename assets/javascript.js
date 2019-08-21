06b0a784c1acd412170b74479b98f2b393da82d0
jon <jcrossguitar@gmail.com>
2019-08-21 13:30:32 -0700

    I reached a point where I could not move forward. I looked at the solutions to help me, but everything I have added so far has not changed any of my results.

--- assets/javascript.js
+++ assets/javascript.js
@@ -12,12 +12,9 @@ for (var i = 0; i < topics.length; i++) {
 var createRow = function(data) {
     // Create a new table row element
     var tRow = $("<tr>");
-
     // Methods run on jQuery selectors return the selector they we run on
     // This is why we can create and save a reference to a td in the same statement we update its text
     var titleTd = $("<td>").text(data);
-    
-      
     // Append the newly created table data to the table row
     tRow.append(titleTd);
     // Append the table row to the table body
@@ -38,7 +35,30 @@ $(document).ready(function() {
                  
             xhr.done(function(data) { console.log("success got data", data); })
             .then(function(response) {
-                createRow(response);
+                var results = response.data;
+                // createRow(response);
+                //this area i took from the solutions because the way i was attempting was not working. 
+                for (var i = 0; i < results.length; i++) {
+                    var animals = $("<div class=\"animal-item\">");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
+
+                    var animalImage = $("<img>");
+                    animalImage.attr("src", still);
+                    animalImage.attr("data-still", still);
+                    animalImage.attr("data-animate", animated);
+                    animalImage.attr("data-state", "still");
+                    animalImage.addClass("animal-image");
+
+                    animalDiv.append(p);
+                    animalDiv.append(animalImage);
+
+                    $(".table").append(animalDiv);
+
+
+                }
             });
                  
                             // this area is my function for creating buttons, but is not working at the moment
@@ -59,12 +79,12 @@ $(document).ready(function() {
                     console.log('search...')
                             // creating a variable for search term
         var search = $("#topic").val();
-            //  // this area is my function for creating buttons, but is not working at the moment
-            // $("#inputBtn").click(function myFunction() {
-            //     var btn = document.createElement(search);
-            //     btn.innerHTML = "";
-            //     document.body.appendChild("#topic")
-            // });
+             // this area is my function for creating buttons, but is not working at the moment
+            $("#inputBtn").click(function myFunction() {
+                var btn = document.getElementById(search);
+                search.innerHTML = "";
+                document.body.appendChild("#topics")
+            });
         
 
         var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=qs2bKebUVChguz65nBpVv6CbyYU8EYH6&limit=10");
